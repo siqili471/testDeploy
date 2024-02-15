02baa9386ce7e7ef1830a9301b8879278a37ca9f
Siqi Li <1114673087@qq.com>
2024-02-15 13:51:02 +0000

    "f"

--- frontend/src/components/forms/ClassroomForm.js
+++ frontend/src/components/forms/ClassroomForm.js
@@ -14,7 +14,7 @@ const ClassroomForm = () => {
 
         const classroom = {title, owner}
 
-        const response = await fetch('http://localhost:4000/classrooms', {
+        const response = await fetch(`${process.env.HOST}/classrooms`, {
             method: 'POST',
             body: JSON.stringify(classroom),
             headers: {
